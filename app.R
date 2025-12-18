180d0b9e9d4bec2934c77276c7ba7c57a34c16a7
bm743 <bm743@cornell.edu>
2025-12-18 14:41:55 -0500

    Add UI, render tables, create server

---
+++ app.R
@@ -0,0 +1,87 @@
+source("R/mod_data_input.R")
+source("R/mod_filtering.R")
+source("R/mod_pca_compute.R")
+source("R/mod_pca_plot.R")
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      mod_data_input_ui("data"),
+      mod_filter_ui("filter")
+    ),
+    mainPanel(
+      tabsetPanel(
+        id = "tabs",
+        tabPanel("PCA Plot", mod_pca_plot_ui("pca")),
+        tabPanel("Scree Plot", mod_scree_plot_ui("pca")),
+        tabPanel("PC Scores", DT::DTOutput("pc_table")),
+        tabPanel("Loadings", DT::DTOutput("loading_table"))
+        
+      )
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+  data <- mod_data_input_server(
+    "data",
+    example_expression,
+    example_metadata
+  )
+  
+  expr_filt <- mod_filter_server("filter", data$expr_raw)
+  
+  pca <- mod_pca_server("pca_compute", expr_filt)
+  
+  mod_pca_plot_server(
+    "pca",
+    expr_filt,
+    pca$pca_res,
+    pca$variance
+  )
+  
+  # PC SCORES TABLE (samples x PCs)
+  output$pc_table <- renderDT({
+    req(pca$pca_res())
+    
+    
+    scores <- as.data.frame(pca$pca_res()$x)
+    scores$sample <- rownames(scores)
+    
+    
+    scores <- scores[, c("sample", colnames(scores))]
+    
+    
+    datatable(
+      scores,
+      options = list(
+        pageLength = 10,
+        scrollX = TRUE
+      )
+    )
+  })
+  
+  # LOADINGS TABLE (genes x PCs)
+  output$loading_table <- renderDT({
+    req(pca$pca_res())
+    
+    
+    loadings <- as.data.frame(pca$pca_res()$rotation)
+    loadings$gene <- rownames(loadings)
+    
+    
+    loadings <- loadings[, c("gene", colnames(loadings))]
+    
+    
+    datatable(
+      loadings,
+      options = list(
+        pageLength = 10,
+        scrollX = TRUE
+      )
+    )
+  })
+}
+
+shinyApp(ui, server)
